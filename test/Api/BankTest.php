10b7ecefaf8ed9e2d9272ffd63e41685078b2675
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-07 13:35:09 +0100

    Add More Test

---
+++ test/Api/BankTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *
+ * This file is part of the Xeviant Paystack package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package          Paystack
+ * @version          1.0
+ * @author           Olatunbosun Egberinde
+ * @license          MIT Licence
+ * @copyright    (c) Olatunbosun Egberinde <bosunski@gmail.com>
+ * @link             https://github.com/bosunski/paystack
+ *
+ */
+
+namespace Xeviant\Paystack\Tests\Api;
+
+
+class BankTest extends ApiTestCase
+{
+	const PATH = '/page';
+
+	/**
+	 * @test
+	 */
+	public function shouldGetBanks(): void
+	{
+		$expectedResult = ['data' => [['name' => 'Access Bank']]];
+
+		$api = $this->getApiMock();
+		$api->expects(self::once())
+		    ->method('get')
+		    ->with(self::PATH)
+		    ->willReturn($expectedResult);
+
+		$this->assertEquals($expectedResult, $api->list());
+	}
+
+
+	/**
+	 * @test
+	 */
+	public function shouldGetTransactionsApiObject()
+	{
+		$api = $this->getApiMock();
+
+		self::assertInstanceOf(Bank::class, $api);
+	}
+
+
+	/**
+	 * @return string
+	 */
+	protected function getApiClass(): string
+	{
+		return Bank::class;
+	}
+
+}
