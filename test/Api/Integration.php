2b80a4369711aa8f7e1a1fd8c84c4d5af213d1db
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-08 15:30:21 +0100

    Add More Test

---
+++ test/Api/Integration.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * This file is part of the Xeviant Paystack package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package          Paystack
+ * @version          1.0
+ * @author           Olatunbosun Egberinde
+ * @license          MIT Licence
+ * @copyright    (c) Olatunbosun Egberinde <bosunski@gmail.com>
+ * @link             https://github.com/bosunski/paystack
+ *
+ */
+
+namespace Xeviant\Paystack\Tests\Api;
+
+
+use Xeviant\Paystack\Api\AbstractApi;
+
+class Integration extends AbstractApi
+{
+	const BASE_PATH = '/integration';
+
+	public function fetchPaymentSessionTimeout()
+	{
+		return $this->get(self::BASE_PATH . "/payment_session_timeout");
+	}
+
+	public function updatePaymentSessionTimeout(array $parameters)
+	{
+		return $this->put(self::BASE_PATH . "/payment_session_timeout", $parameters);
+	}
+}
