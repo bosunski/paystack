011a55977c2486669d289ae991d433ba8e20fd3f
Olatunbosun Egberinde <bosunski@gmail.com>
2019-02-10 03:27:41 +0100

    Add @method docs

--- src/Paystack.php
+++ src/Paystack.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  *
  * @package         Paystack
- * @version         2.0
+ * @version         1.0
  * @author          Olatunbosun Egberinde
  * @license         MIT Licence
  * @copyright       (c) Olatunbosun Egberinde <bosunski@gmail.com>
@@ -17,6 +17,26 @@
  */
 
 namespace Xeviant\Paystack;
+
+/**
+ * @method \Xeviant\Paystack\Api\Customers customers()
+ * @method \Xeviant\Paystack\Api\Balance balance()
+ * @method \Xeviant\Paystack\Api\Bank bank()
+ * @method \Xeviant\Paystack\Api\BulkCharges bulkCharges()
+ * @method \Xeviant\Paystack\Api\Bvn bvn()
+ * @method \Xeviant\Paystack\Api\Charge charge()
+ * @method \Xeviant\Paystack\Api\Integration integration()
+ * @method \Xeviant\Paystack\Api\Invoices invoices()
+ * @method \Xeviant\Paystack\Api\Pages pages()
+ * @method \Xeviant\Paystack\Api\Plans plans()
+ * @method \Xeviant\Paystack\Api\Refund refund()
+ * @method \Xeviant\Paystack\Api\Settlements settlements()
+ * @method \Xeviant\Paystack\Api\SubAccount subAccount()
+ * @method \Xeviant\Paystack\Api\Subscriptions subscriptions()
+ * @method \Xeviant\Paystack\Api\Transactions transactions()
+ * @method \Xeviant\Paystack\Api\TransferRecipients transferRecipients()
+ * @method \Xeviant\Paystack\Api\Transfers transfers()
+ */
 
 class Paystack
 {
