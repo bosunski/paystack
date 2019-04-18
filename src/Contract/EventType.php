6ccb098dd37d8c87e24c2c0bd761bc10a4f908b3
Olatunbosun Egberinde <bosunski@gmail.com>
2019-04-18 18:39:57 +0100

    Update EventTYpe

--- src/Contract/EventType.php
+++ src/Contract/EventType.php
@@ -7,4 +7,11 @@ interface EventType
 {
     const INVOICE_UPDATED = 'invoice.update';
     const INVOICE_CREATED = 'invoice.create';
+    const CHARGE_SUCCESS = 'charge.success';
+    const TRANSFER_SUCCESS = 'transfer.success';
+    const TRANSFER_FAILED = 'transfer.failed';
+    const INVOICE_FAILED = 'invoice.failed';
+    const SUBSCRIPTION_ENABLED = 'subscription.enable';
+    const SUBSCRIPTION_DISABLED = 'subscription.disable';
+    const SUBSCRIPTION_CREATE = 'subscription.disable';
 }
